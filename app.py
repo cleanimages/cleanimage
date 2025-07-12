5c25545a563dab44fdd674ba9a34adbb49ff4663
cleanimages <fiston039@gmail.com>
2025-07-13 00:29:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -71,11 +71,11 @@ def launch_gradio():
 def home():
     return render_template("index.html")
 
+# Démarrer Gradio dans un thread séparé
 threading.Thread(target=launch_gradio).start()
 
+# Point d'entrée correct pour Render
 if __name__ == "__main__":
     import os
-
-port = int(os.environ.get('PORT', 5000))
-app.run(host="0.0.0.0", port=port)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)

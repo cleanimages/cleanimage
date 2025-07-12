d60116ab29c4939a60252dd44a5b17e7346eda3a
cleanimages <fiston039@gmail.com>
2025-07-12 23:48:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,8 @@ def home():
 threading.Thread(target=launch_gradio).start()
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    import os
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host="0.0.0.0", port=port)
+
